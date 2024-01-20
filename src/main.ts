f534f7327eb47bb788c5ac2e389a75f796ee6b51
PiterDev <piterzdev@gmail.com>
2024-01-20 19:57:54 +0100

    examples updated

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import "./index.css";
 import UI from "@UI";
 import Counter from "./Counter";
 import Todo from "./Todo";
-import Counter_2 from "./Counter_2";
 
 UI.HandleStateFull(Counter, document.getElementById("app") ?? document.body);
 
 UI.HandleStateFull(Todo, document.getElementById("app") ?? document.body);
 
-UI.HandleStateFull(Counter_2, document.getElementById("app") ?? document.body);
 
 // UI.HandleStateFull(
 //     FragmentTest,
