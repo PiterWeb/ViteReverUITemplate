9fc9818393bd3e887b2570d241d0b816032f33b1
PiterWeb <piterzdev@gmail.com>
2024-10-02 21:41:10 +0200

    updated reverouter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "./index.css";
-
+import { $UI } from "reverui";
 import { $lazy, $Router } from "reverouter";
 
-$Router({
+$Router($UI, {
 	"/": $lazy(() => import("./Banner")),
 	"/todo": $lazy(() => import("./Todo")),
 	"/counter": $lazy(() => import("./Counter")),
